dd7706c69eb9a01a2d6a8d0cd8bb83b91d24f5f3
albert-herbst <albert.herbst99@gmail.com>
2023-12-01 19:50:28 +0100

    Basic K8 deployment of frontend.

---
+++ build.ps1
@@ -0,0 +1,32 @@
+# Function to build Docker image and tag it
+function BuilDockeImage($directory) {
+    $imageName = "albh/converter-app:" + $directory.ToLower()
+    
+    # Check if Dockerfile exists in the specified directory
+    $dockerfilePath = Join-Path $rootDirectory $directory
+    if (Test-Path "$dockerfilePath\Dockerfile") {
+        Write-Host "Building Docker image for $directory..."
+        
+        # Run docker build and tag the image
+        docker build -t $imageName $dockerfilePath
+        
+        Write-Host "Docker image built and tagged: $imageName"
+    }
+    else {
+        Write-Host "No Dockerfile found in $directory."
+    }
+}
+
+# Main script
+$rootDirectory = Get-Location
+
+# Get all subdirectories
+$subdirectories = Get-ChildItem -Directory -Path $rootDirectory | Select-Object -ExpandProperty Name
+
+# Iterate through each subdirectory
+foreach ($subdirectory in $subdirectories) {
+    # Call the function to build Docker image for each subdirectory
+    BuilDockeImage $subdirectory
+}
+
+# docker-compose up
